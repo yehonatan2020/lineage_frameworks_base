40da92d7371f96df4ee8ab4537bc1b1b248f3768
yehonatan2020 <yehonatan@yandex.ru>
2024-03-10 20:14:09 +0000

    fix compilation

--- packages/SystemUI/src/com/android/systemui/qs/tiles/AmbientDisplayTile.java
+++ packages/SystemUI/src/com/android/systemui/qs/tiles/AmbientDisplayTile.java
@@ -57,7 +57,7 @@ public class AmbientDisplayTile extends QSTileImpl<BooleanState> {
     public static final String TILE_SPEC = "ambient_display";
 
     private final Icon mIcon = ResourceIcon.get(R.drawable.ic_qs_ambient_display);
-    private final SecureSetting mSetting;
+    private final SettingObserver mSetting;
 
     @Inject
     public AmbientDisplayTile(
